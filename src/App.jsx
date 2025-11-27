47afd8f45b37f4cf6ea15f5f1415208309cef583
Agustin Tabarcache <agustintabarcache74@gmail.com>
2025-11-27 15:40:11 -0300

    arreglo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
       console.log(valor);
     })
 
-    const urlBack = 'http://localhost:8080/api/v1/uploads/'
+    const urlBack = 'https://prueba-back-upload-images.onrender.com/api/v1/uploads/'
     const options = {
       method: 'POST',
       body: formData
